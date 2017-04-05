70bc3007f920a8aa10bfb87976d9b19b65c6abd4
MiguelR92 <miguelguijarro@MacBook-Pro-de-Miguel.local>
2017-04-04 20:11:43 -0500

    first

---
+++ src/lib/color.js
@@ -0,0 +1,10 @@
+
+const defaultTheme = {
+  leftPanelBg: '#333',
+  leftPanelText: '#fff',
+  rightPanelBg: '#F5F8FA',
+  rightPanelText: '#202B33',
+  grey: '#CED9E0',
+}
+
+module.exports = { defaultTheme }
